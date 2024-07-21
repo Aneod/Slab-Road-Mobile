f201b5f3dcf0f186bdefda678e12e7ddb316debe
Aneod <blysboutique@gmail.com>
2024-07-21 03:16:24 +0200

    PopUp testing

--- app/src/main/java/com/example/veritablejeu/PopUp/InlineComponent/Preset/SimpleText.java
+++ app/src/main/java/com/example/veritablejeu/PopUp/InlineComponent/Preset/SimpleText.java
@@ -12,18 +12,22 @@ import com.example.veritablejeu.PopUp.PopUp;
 @SuppressLint("ViewConstructor")
 public class SimpleText extends InlineComponent {
 
+    private static final int DEFAULT_GRAVITY = Gravity.NO_GRAVITY;
+
     private final Text textView;
 
     public SimpleText(@NonNull PopUp popUp, String text) {
-        super(popUp);
-        textView = new Text(this, text, getLayoutParams().width, 0, Gravity.NO_GRAVITY);
-        addView(textView);
+        this(popUp, text, DEFAULT_GRAVITY);
     }
 
     public SimpleText(@NonNull PopUp popUp, String text, int gravity) {
         super(popUp);
         textView = new Text(this, text, getLayoutParams().width, 0, gravity);
         addView(textView);
+    }
+
+    public Text getTextView() {
+        return textView;
     }
 
     public void setText(String text) {
