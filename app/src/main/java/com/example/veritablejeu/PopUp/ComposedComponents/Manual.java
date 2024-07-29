5369a3267777f45e47a6c8399992730e2497f431
Aneod <blysboutique@gmail.com>
2024-07-29 09:23:11 +0200

    French to English manual

--- app/src/main/java/com/example/veritablejeu/PopUp/ComposedComponents/Manual.java
+++ app/src/main/java/com/example/veritablejeu/PopUp/ComposedComponents/Manual.java
@@ -35,78 +35,78 @@ public class Manual {
 
         return new InlineComponent[]{
 
-                new SimpleText(popUp, "Gameplay très simple :\n\n" +
-                        "Cliquer sur un blob pour le sélectionner. Il changera alors de couleur."),
+                new SimpleText(popUp, "Very simple gameplay\n :\n\n" +
+                        "Click on a blob to select it. It will then change color."),
 
                 new SimpleImage(popUp, R.drawable.img1),
 
-                new SimpleText(popUp, "Puis cliquer sur une des cases du jeu pour qu’il s’y déplace."),
+                new SimpleText(popUp, "Then click on one of the squares in the game so that it moves there."),
 
                 new SimpleImage(popUp, R.drawable.img2),
 
-                new SimpleText(popUp, "Objectif simple :\n" +
-                        "Vous trouverez des cases oranges sur le terrain.\n" +
-                        "Le jeu est terminé si toutes les cases oranges sont complétées.\n" +
-                        "Une case est complétée tant qu'il y a autant de blob dessus que le nombre de point."),
+                new SimpleText(popUp, "Simple objective :\n" +
+                        "You will find orange slabs on the ground.\n" +
+                        "The game is over if all the orange slabs are completed.\n" +
+                        "A square is completed as long as there are as many blobs on it as the number of points."),
 
                 new SimpleImage(popUp, R.drawable.img3),
 
-                new SimpleText(popUp, "S’il n’y a aucun point, alors un seul blob suffit.\n" +
-                        "Mais il y a des murs entre vos blobs et ces cases oranges.\n" +
-                        "Les blobs ne traversent pas les murs."),
+                new SimpleText(popUp, "If there are no points, then a single blob is enough.\n" +
+                        "But there are walls between your blobs and these orange slabs.\n" +
+                        "Blobs do not pass through walls."),
 
                 new SimpleImage(popUp, R.drawable.img5),
 
-                new SimpleText(popUp, "En revanche les murs sont parsemés de portes que vous pouvez ouvrir."),
+                new SimpleText(popUp, "However, the walls are dotted with doors that you can open."),
 
                 new SimpleImage(popUp, R.drawable.img6),
 
-                new SimpleText(popUp, "Comment ouvrir les portes ?\n" +
-                        "Chaque porte est connectée à une ou plusieurs cases."),
+                new SimpleText(popUp, "How to open the doors?\n" +
+                        "Each door is connected to one or more slabs."),
 
                 new SimpleImage(popUp, R.drawable.img7),
 
-                new SimpleText(popUp, "Mettre un blob sur une case active cette case. Et retirer un blob de la case la désactive."),
+                new SimpleText(popUp, "Placing a blob on a slab activates that slab. And removing a blob from the slab deactivates it."),
 
                 new SimpleImage(popUp, R.drawable.img8),
 
-                new SimpleText(popUp, "Une porte reste ouverte tant qu’il y a assez de cases connectées activées.\n\n" +
-                        "Les portes bleues clairs s’ouvrent si au moins une case est activée."),
+                new SimpleText(popUp, "A door remains open as long as there are enough connected slabs activated.\n\n" +
+                        "Light blue doors open if at least one slab is activated."),
 
                 new SimpleImage(popUp, R.drawable.img9),
 
-                new SimpleText(popUp, "Au moins deux pour les bleues foncées."),
+                new SimpleText(popUp, "At least two for dark blue ones."),
 
                 new SimpleImage(popUp, R.drawable.img10),
 
-                new SimpleText(popUp, "Les portes rouges s’ouvrent si au moins une case rouge est activée.\n" +
-                        "Mais les cases rouges sont plus grandes et nécessitent plusieurs blobs pour être activées."),
+                new SimpleText(popUp, "Red doors open if at least one red slab is activated.\n" +
+                        "But the red slabs are larger and require several blobs to activate."),
 
                 new SimpleImage(popUp, R.drawable.img11),
 
-                new SimpleText(popUp, "Les cases spéciales :\n" +
-                        "Il existe des cases qui n’ouvrent pas de portes mais qui ont des effets spéciaux.\n" +
-                        "Et une fois activés, ces cases ne se désactivent pas.\n\n" +
-                        "Parfois, vous trouverez des portes vertes. Ces portes ne pourront êtres" +
-                        "ouvertes tant qu’elles auront cette couleur. Mais en activant une case verte, " +
-                        "vous rendrez à ces portes leurs couleurs normales."),
+                new SimpleText(popUp, "Special slabs :\n" +
+                        "There are slabs that do not open doors but have special effects.\n" +
+                        "And once activated, these slabs do not deactivate.\n\n" +
+                        "Sometimes you will find green doors. These doors cannot be " +
+                        "open as long as they have this color. But by activating a green slab, " +
+                        "you will return these doors to their normal colors."),
 
                 new SimpleImage(popUp, R.drawable.img12),
 
-                new SimpleText(popUp, "Il sera alors possible de les ouvrir.\n" +
-                        "En activant une case jaune, vous obtiendrez autant de blob que le nombre de " +
-                        "points sur la case. Et s’il n’y en a pas, alors le nombre de blob que vous " +
-                        "obtiendrez est de un."),
+                new SimpleText(popUp, "It will then be possible to open them.\n" +
+                        "By activating a yellow slab, you will obtain as many blobs as the number of " +
+                        "points on the slab. And if there is none, then the number of blobs you " +
+                        "will get is one."),
 
                 new SimpleImage(popUp, R.drawable.img13),
 
-                new SimpleText(popUp, "Enfin, il existe les cases violettes. A proximité vous trouverez des " +
-                        "cases transparentes impossibles à atteindre."),
+                new SimpleText(popUp, "Finally, there are the purple slabs. Nearby you will find " +
+                        "transparent slabs impossible to reach."),
 
                 new SimpleImage(popUp, R.drawable.img14),
 
-                new SimpleText(popUp, "En activant une case violette, vous rendez possible l’accès aux \n" +
-                        "cases transparents adjacentes."),
+                new SimpleText(popUp, "By activating a purple slab, you make it possible to access the \n" +
+                        "adjacent transparent slabs."),
 
                 new SimpleImage(popUp, R.drawable.img15)
         };
